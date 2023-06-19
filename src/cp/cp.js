ad6bb942fcf21375be22459c01ce1b6f586c12ca
Yu Mentsel <yu.mentsel@gmail.com>
2023-06-20 02:24:14 +0300

    feat: add implementation with { silent: true } for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,7 +6,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const spawnChildProcess = async (args) => {
   const file = join(__dirname, 'files', 'script.js');
+
+  //implementation option 1
   fork(file, args);
+
+  //or implementation option 2
+  // const child = fork(file, args, { silent: true });
+  // process.stdin.pipe(child.stdin);
+  // child.stdout.pipe(process.stdout);
 };
 
 spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
