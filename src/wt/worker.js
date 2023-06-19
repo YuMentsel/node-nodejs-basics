ad747a3fd1f8989349986392853eb202277c90fa
Yu Mentsel <yu.mentsel@gmail.com>
2023-06-20 01:49:10 +0300

    fix: clear fileToWrite.txt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,4 +1,4 @@
-import { parentPort, workerData } from 'worker_threads';
+import { parentPort, workerData } from 'node:worker_threads';
 
 // n should be received from main thread
 const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
