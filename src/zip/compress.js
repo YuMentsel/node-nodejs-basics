ab0c188662d2ec1fcf0219d8ddb3ccb29f268730
Yu Mentsel <yu.mentsel@gmail.com>
2023-06-19 21:24:28 +0300

    feat: implement functions for zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const txtFile = join(__dirname, 'files', 'fileToCompress.txt');
+  const gzFile = join(__dirname, 'files', 'archive.gz');
+
+  const gzip = createGzip();
+  const read = createReadStream(txtFile);
+  const write = createWriteStream(gzFile);
+  read.pipe(gzip).pipe(write);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const gzFile = join(__dirname, 'files', 'archive.gz');
+  const txtFile = join(__dirname, 'files', 'fileToCompress.txt');
+
+  const gunzip = createGunzip();
+  const read = createReadStream(gzFile);
+  const write = createWriteStream(txtFile);
+  read.pipe(gunzip).pipe(write);
 };
 
 await decompress();
