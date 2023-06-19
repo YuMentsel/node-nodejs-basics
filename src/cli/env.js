db7d1876978a01c08e4a72a5fe6ea2451389a42b
Yu Mentsel <yu.mentsel@gmail.com>
2023-06-19 14:32:30 +0300

    feat: implement functions for cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envArr = Object.entries(process.env).reduce((acc, [key, val]) => {
+    if (key.startsWith('RSS_')) {
+      return [...acc, `${key}=${val}`];
+    }
+    return acc;
+  }, []);
+
+  if (envArr.length) {
+    console.log(envArr.join(', '));
+  }
 };
 
 parseEnv();
